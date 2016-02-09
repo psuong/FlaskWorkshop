dbdffeb17a9d0bd907fd14590be9a568987900d5
Porrith Suong <psuong95@gmail.com>
2016-02-09 12:02:51 -0500

    Changes python file in output-string to be PEP8 compliant and defines the port.

--- output-string/app.py
+++ output-string/app.py
@@ -1,22 +1,27 @@
 # If you're not used to Python syntax, what the line below is stating is
 # from the flask file import a specific module called Flask.
-# For those used to C++ or any C like language, think of it as the "using" keyword.
+# For those used a C like language, think of it as the "using" keyword.
 from flask import Flask
 
 # The line below creates an object called app.
-# The __name__ is used for single web apps! This lets Flask know which directories to look at.
+# The __name__ is used for single web apps! This lets Flask know which
+# directories to look at.
 app = Flask(__name__)
 
 # Line 15 contains a decorator due to the @ symbol. So what are decorators?
-# Decorators are simply ways of extending a function's functionality without directly having to change
-# the function.
-# @app.route('/') has an innate functionality to provide the URL of a specific page, while the function
-# def index() provides the functionality to return a string to be rendered at that specific URL.
+# Decorators are simply ways of extending a function's functionality without
+# directly having to change the function.
+# @app.route('/') has an innate functionality to provide the URL of a specific
+# page, while the function def index() provides the functionality to return a
+# string to be rendered at that specific URL.
+
+
 @app.route('/')
 def index():
     return "Congratulations! You just launched a flask app!"
 
-# The following if statement below lets Python know that this specific file is the main file to execute.
+# The following if statement below lets Python know that this specific file is
+# the main file to execute.
 if __name__ == '__main__':
-    # Allow some debugging so you'll know what's wrong!
-    app.run(debug=True)
+    # Let the app run on a port at address 5000 and allow some debugging.
+    app.run(port=5000, debug=True)
