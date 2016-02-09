20cf07e3938b651fb7ec0e7b2f77aca42e93d1b0
Porrith Suong <psuong95@gmail.com>
2016-02-09 11:58:32 -0500

    Modifies the file to be PEP-8 compliant.

--- flask-templating/app.py
+++ flask-templating/app.py
@@ -1,6 +1,6 @@
-# In order for Flask to use templates, you need to import another module called "render_template"
-# Essentially, "render_template" does exactly what it sounds like; it takes an HTML template file and
-# loads it onto a webpage.
+# In order for Flask to use templates, you need to import another module called
+# "render_template" Essentially, "render_template" does exactly what it sounds
+# like; it takes an HTML template file and loads it onto a webpage.
 from flask import Flask, render_template
 
 
@@ -9,33 +9,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # The following return statement below renders the base.html file and locates each variable within
-    # the html files. Assign each variable a value and run the app. Your browser will display the contents
+    # The following return statement below renders the base.html file and
+    # locates each variable within the html files. Assign each variable a
+    # value and run the app. Your browser will display the contents
     # you've written for each variable.
 
-    # TODO: Add the line number the reader should reference.
-    # Note: If you are missing a variable that needs to be assigned Flask will spout an error in your terminal
-    # and on the webpage as long as debugging is enabled! (See line )
+    # Note: If you are missing a variable that needs to be assigned Flask
+    # will spout an error in your terminal and on the webpage as long as
+    # debugging is enabled! (See line )
 
-    # Play around with the values if you feel like you need to try it for yourself. :)
-    return render_template('base.html', title="Showcasing Templating!", 
-                                        body_content="This content will be loaded into the "
-                                        "base.html file whose variable name is \"body_content\"!")
+    # Modify the strings if you'd like to try it for yourself. :)
+    return render_template('base.html', title="Showcasing Templating!",
+                           body_content="This content will be loaded into the "
+                                        "base.html file whose variable name is"
+                                        "\"body_content\"!")
 
 
 @app.route('/clean-variables')
 def cleaner_variable_loading():
-    # TODO: Show kwargs using a dict rather than getting every variable in HTML files and assigning it in the 
-    # return statement.
-    # Instead of assigning your variables in your return statement you can place them in a dictionary and use
-    # Python's keyword arguments to assign the variables.
-    content_dict = {"title":"Using a Dictionary Instead",
-                    "body_content":"Simply create a dictionary with all of your variable names as the keys "
-                                   "and variable assignments as the dictionary values. Pass the dictionary into"                                   " the return statement of the render_template as a keyword argument, like "
-                                   " this: **dictionary."
-                   }
+    # Instead of assigning your variables in your return statement you can
+    # place them in a dictionary and use# Python's keyword arguments to assign
+    # the variables.
+    content_dict = {"title": "Using a Dictionary Instead",
+                    "body_content": "Simply create a dictionary with all of "
+                    "your variable names as the keys and variable assignments "
+                    "as the dictionary values. Pass the dictionary into "
+                    "the return statement of the render_template as a keyword "
+                    "argument, like this: **dictionary."
+                    }
     return render_template('base.html', **content_dict)
 
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
