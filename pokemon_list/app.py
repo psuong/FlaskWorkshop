836c5653c5a18a036057bbbb7df14c06607ebf22
Daniel Chia <daniel_1118@hotmail.com>
2016-02-13 22:41:36 -0500

    added view to handle posts, set debug true and element unique to false

--- pokemon_list/app.py
+++ pokemon_list/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 # import Pokemon class, add create some instances, add them and commit
 class Pokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(20), unique=False)
     element = db.Column(db.String(20), unique=True)
 
     def __init__(self, name, element):
@@ -33,7 +33,25 @@ def pokemons():
     return render_template("index.html", pokemons=pokemons)
 
 
+@app.route('/add_pokemon', methods=['POST'])
+def new_pokemon():
+
+    name = request.form['name']
+    element = request.form['element']
+
+    new_pokemon = Pokemon(name, element)
+    db.session.add(new_pokemon)
+    db.session.commit()
+
+    return redirect('/pokemons')
+
+
 if __name__ == '__main__':
-    app.run()
+    # always have debug true while developing
+    app.run(debug=True)
+
+
+
+
 
 
